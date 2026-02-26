ea1849d146da0ec38673e990f0f0d2f5c56c5d9d
Anand Suthar <anand.suthar.cg@gmail.com>
2026-02-26 13:36:25 +0530

    refactor routes and improve user handling in the Express server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const users = [
 {
@@ -29,29 +30,34 @@ app.get("/", (req, res) => {
   res.send("Express server is running");
 });
 
-app.get("/cg", (req, res) => {
+app.get("/users", (req, res) => {
     console.log("users",users)
   res.status(200).json(users);
 });
 
-app.get("/cg/students", (req, res) => {
+app.get("/users/cg", (req, res) => {
+    console.log("users",users)
+  res.status(200).json(users);
+});
+
+app.get("/users/cg/students", (req, res) => {
     console.log("users",users)
   res.status(200).json(users);
 });
 
 
-// app.get("/cg/students/gr/:gr_number", (req, res) => {
-//   const grNumber = req.params.gr_number;
-//   const user = users.find(u => u.UniversityUID === grNumber);
+app.get("/users/cg/students/gr/:gr_number", (req, res) => {
+  const grNumber = req.params.gr_number;
+  const user = users.find(u => u.UniversityUID === grNumber);
 
-//   if (!user) {
-//     return res.status(404).json({ message: "User not found" });
-//   }
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-//   res.status(200).json(user);
-// });
+  res.status(200).json(user);
+});
 
-app.get("/cg/students/name/:name", (req, res) => {
+app.get("/users/cg/students/name/:name", (req, res) => {
   const name = req.params.name;
   const user = users.find(u => u.studentName.toLowerCase() === name.toLowerCase());
 
@@ -63,7 +69,20 @@ app.get("/cg/students/name/:name", (req, res) => {
 });
 
 
+app.post("/users", (req, res) => {
+  const newUser = {
+    studentName: req.body.studentName,
+    University: req.body.University,
+    UniversityUID: req.body.UniversityUID
+  };
 
+  users.push(newUser);
+
+  res.status(201).json({
+    message: "User created",
+    user: newUser
+  });
+});
 
 
 app.listen(3000, () => {
